e1b304a68887046230029349690ab36e391253f2
Marcos <marcosrivas358@gmail.com>
2025-09-06 21:01:44 -0400

    merge conflict

--- Game.py
+++ Game.py
@@ -19,7 +19,7 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 7
+    speed = 10
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
@@ -34,10 +34,6 @@ def ball_movement():
             ball_speed_y *= -1  # Reverse ball's vertical direction
             hit_sound.play()
             # TODO Task 6: Add sound effects HERE
-
-#pygame.init()
-#pygame.mixer.init()
-#hit_sound = pygame.mixer.Sound("hit_sound.wav")
 
 
     # Ball collision with top boundary
