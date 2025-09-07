e6fab6bf8bd86a2fe76f756741aefa71eed0bafb
Marcos <marcosrivas358@gmail.com>
2025-09-06 21:23:56 -0400

    Task #5 complete: ball speed increments when touching paddle.

--- Game.py
+++ Game.py
@@ -19,7 +19,7 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 10
+    speed = 7
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
@@ -31,13 +31,9 @@ def ball_movement():
             # TODO Task 2: Fix score to increase by 1
             #Task 2: Completed
             score += 1  # Increase player score
-            ball_speed_y *= -1  # Reverse ball's vertical direction
+            ball_speed_y *= -1.1  # Reverse ball's vertical direction
             hit_sound.play()
             # TODO Task 6: Add sound effects HERE
-
-#pygame.init()
-#pygame.mixer.init()
-#hit_sound = pygame.mixer.Sound("hit_sound.wav")
 
 
     # Ball collision with top boundary
